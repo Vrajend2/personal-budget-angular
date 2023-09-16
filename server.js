03fef6aa8325ef63d1e77b3c50918edaeef835a6
Vignesh <rvigneshbabu@gmail.com>
2023-09-15 21:01:30 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -6,17 +6,41 @@ app.use('/', express.static('public'));
 
 const budget = [
     { 
-       title :'Eat out',
-       budget:25
-    },
-    {
-       title:'Rent',
-       budget:375
-    },
-    {
-       title:'Grocery',
-       budget:110
-    },
+        title :'Eat out',
+        budget:25
+     },
+     {
+        title:'Rent',
+        budget:150
+     },
+     {
+        title:'Grocery',
+        budget:75
+     },
+     { 
+        title :'Medical and Healthcare',
+        budget:35
+     },
+     {
+        title:'Transportation',
+        budget:45
+     },
+     {
+        title:'Utilities',
+        budget:25
+     },
+     { 
+        title :'Insurance',
+        budget:25
+     },
+     {
+        title:'Childcare',
+        budget:75
+     },
+     {
+        title:'Clothing',
+        budget:25
+     },
   ];
 
 app.get('/hello',(req, res)  => {
