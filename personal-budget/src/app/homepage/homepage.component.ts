8374dbf8a0813983d358ad9fb61487c33db9f7db
Vignesh <rvigneshbabu@gmail.com>
2023-09-24 19:50:47 -0400

    Added Angular Changes

---
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -0,0 +1,64 @@
+import { Component,AfterViewInit } from '@angular/core';
+import {HttpClient, HttpClientModule } from '@angular/common/http';
+import Chart from 'chart.js/auto';
+
+@Component({
+  selector: 'pb-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent implements AfterViewInit {
+
+  public dataSource ={
+    datasets: [
+     {
+        data:[25,150,75,35,45,25,25,75,25],
+        backgroundColor: [
+        '#ffcd56','#ff6384','#36a2eb','#fd6b19', '#f38b4a', '#56d798', '#ff8397','#6970d5','#eb3639'
+        ],
+     }
+   ],
+   labels: [
+      'Eat out',
+      'Rent',
+      'Grocery',
+      'Medical and Healthcare',
+      'Transportation',
+      'Utilities',
+      'Insurance',
+      'Childcare',
+      'Clothing',
+
+   ]
+};
+
+ constructor(private http: HttpClient) {}
+
+ ngAfterViewInit(): void {
+   this.http.get('http://localhost:3000/budget')
+   .subscribe((res: any) => {
+
+    for (var i=0; i < res.myBudget.length; i++)
+                 {
+                  this.dataSource.datasets[0].data[i] = res.myBudget[i].budget;
+                  this.dataSource.labels[i] = res.myBudget[i].title;
+                  this.createChart();
+   }
+   });
+}
+
+createChart() {
+  var ctx = <HTMLCanvasElement>document.getElementById('myChart');
+  var myPieChart = new Chart(ctx, {
+    type :'pie',
+    data:this.dataSource
+  });
+
+   var ctx = <HTMLCanvasElement>document.getElementById('doughnutChart');
+   var myDoughnutChart = new Chart(ctx, {
+      type :'doughnut',
+      data:this.dataSource
+    });
+
+    }
+}
---
+++ personal-budget/src/app/pie-chart/pie-chart.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import Chart from 'chart.js/auto';
+
+
+@Component({
+  selector: 'app-pie-chart',
+  templateUrl: './pie-chart.component.html',
+  styleUrls: ['./pie-chart.component.scss']
+})
+export class PieChartComponent {
+
+}
+
+
+
+
