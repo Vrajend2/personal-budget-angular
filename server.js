5983ba74d6b7cdc3d36fa1f91777a99a51f09098
Vignesh <rvigneshbabu@gmail.com>
2023-09-24 02:35:05 -0400

    CORS changes

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+
+//Budget-API
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
+
+app.use(cors());
+//app.use('/', express.static('public'));
+
 
 const budget = [
     { 
@@ -43,15 +50,19 @@ const budget = [
      },
   ];
 
-app.get('/hello',(req, res)  => {
-    res.send('Hello World!');
+ /* 
+  app.get('/hello',(req, res)  => {
+   res.send('Hello World!');
 });
+
+/** */
 
 app.get('/budget',(req, res)  => {
     res.json(budget);
 });
 
+
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`API served at http://localhost:${port}`)
 
 });
